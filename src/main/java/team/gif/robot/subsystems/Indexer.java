3ff924dadefd835bc0d91525a2a961196d9324e7
Rohan C <17001055@students.sd308.org>
2022-02-07 17:34:05 -0600

    Indexer Subsystem Remove Color Sensing (for now)

--- src/main/java/team/gif/robot/subsystems/Indexer.java
+++ src/main/java/team/gif/robot/subsystems/Indexer.java
@@ -4,16 +4,11 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team.gif.robot.RobotMap;
-
-import com.revrobotics.ColorSensorV3;
 import team.gif.robot.commands.indexer.IndexDefault;
 
-import java.awt.*;
 
 public class Indexer extends SubsystemBase {
     //Hardware config
@@ -22,11 +17,6 @@ public class Indexer extends SubsystemBase {
 
     private static final DigitalInput sOne = new DigitalInput(RobotMap.SENSOR_STAGE_ONE);
     private static final DigitalInput sTwo = new DigitalInput(RobotMap.SENSOR_STAGE_TWO);
-
-    private static final I2C.Port i2c = I2C.Port.kOnboard;
-    private static final I2C.Port i2c2 = I2C.Port.kOnboard;
-    private static final ColorSensorV3 colorSense = new ColorSensorV3(i2c);
-    private static final ColorSensorV3 colorSense2 = new ColorSensorV3(i2c2);
 
     public Indexer() {
         super();
@@ -39,32 +29,6 @@ public class Indexer extends SubsystemBase {
 
     public boolean[] sensorStates() {
         return new boolean[]{sOne.get(), sTwo.get()};
-    }
-
-    public String[] senseColors() {
-        Color detected1 = colorSense.getColor();
-        Color detected2 = colorSense2.getColor();
-        String[] colors = new String[2];
-
-        if(detected1.blue > 200 && detected1.red < 100) {
-            colors[0] = "blue";
-        }
-        if(detected1.red > 200 && detected1.blue < 100) {
-            colors[0] = "red";
-        }
-        if(detected2.blue > 200 && detected2.red < 100) {
-            colors[1] = "blue";
-        }
-        if(detected2.red > 200 && detected1.red < 100) {
-            colors[1] = "red";
-        }
-        if(!sensorStates()[0]) {
-            colors[0] = null;
-        }
-        if(!sensorStates()[1]) {
-            colors[1] = null;
-        }
-        return colors;
     }
 
     public void setIndexMotor(double voltage) {
