409bcb52bdeefd241338ebf7135cb817b11de851
BIll Knopf <billknopfjr@yahoo.com>
2022-02-21 20:19:49 -0600

    Field Test

--- src/main/java/team/gif/robot/Robot.java
+++ src/main/java/team/gif/robot/Robot.java
@@ -45,7 +45,7 @@ import team.gif.robot.subsystems.Shooter;
  * project.
  */
 public class Robot extends TimedRobot {
-    public static final boolean isCompBot = true;
+    public static final boolean isCompBot = false;
 
     private Command autonomousCommand;
     private RobotContainer robotContainer;
