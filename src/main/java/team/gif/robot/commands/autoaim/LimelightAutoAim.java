37b6b2fd44ec9dabd4b456a0386ef723c15eae34
Shrey Mathur <shreym01@gmail.com>
2022-03-24 12:29:59 -0500

    Adapted code to falcons

--- src/main/java/team/gif/robot/commands/autoaim/LimelightAutoAim.java
+++ src/main/java/team/gif/robot/commands/autoaim/LimelightAutoAim.java
@@ -36,10 +36,10 @@ public class LimelightAutoAim extends CommandBase {
 
 //        delayCounter = 0;
         Robot.limelight.setLEDMode(3); // turn on - just in case they were turned off somehow
-        Drivetrain.leftTalon1.enableCurrentLimit(false);
-        Drivetrain.leftTalon2.enableCurrentLimit(false);
-        Drivetrain.rightTalon1.enableCurrentLimit(false);
-        Drivetrain.rightTalon2.enableCurrentLimit(false);
+//        Drivetrain.leftTalon1.enableCurrentLimit(false);
+//        Drivetrain.leftTalon2.enableCurrentLimit(false);
+//        Drivetrain.rightTalon1.enableCurrentLimit(false);
+//        Drivetrain.rightTalon2.enableCurrentLimit(false);
 
 
 
@@ -105,12 +105,12 @@ public class LimelightAutoAim extends CommandBase {
 
         if(robotHasSettled){ // Note: can't combine this using else because robotHasSettled can be set to true in the above section
 
-            double xOffset = Robot.limelight.getXOffset();
-            double pivVolts = xOffset * 0.01 * Constants.Shooter.MAX_PIVOT_VOLTS;
+            //double xOffset = Robot.limelight.getXOffset();//commented bc as the robot turns the amount of ring detected change meaning this value needs to change too
+            //double pivVolts = Robot.limelight.getXOffset() * 0.01 * Constants.Shooter.MAX_PIVOT_VOLTS;
 
             if (targetLocked) {
                 // we need to check again to make sure the robot hasn't overshot the target
-                if (xOffset > -1.0 && xOffset < 1.0) {
+                if (Robot.limelight.getXOffset() > -1.0 && Robot.limelight.getXOffset() < 1.0) {
                     if (Robot.shooter.isInTolerance()) {
                         // fire away!
                         System.out.println("Shooting - I hope it went in");
@@ -121,17 +121,17 @@ public class LimelightAutoAim extends CommandBase {
                         System.out.println("Robot is settled and locked. Flywheel not in tolerance.");
                     }
                 } else {
-                    System.out.println("Offset Adjusting at: " + xOffset);
+                    System.out.println("Offset Adjusting at: " + Robot.limelight.getXOffset());
                     // need to relock
                     targetLocked = false;
                     robotHasSettled = false;
                 }
             } else {
-                if (xOffset > -1.0 && xOffset < 1.0) { // target is locked
+                if (Robot.limelight.getXOffset() > -1.0 && Robot.limelight.getXOffset() < 1.0) { // target is locked
                     Robot.drivetrain.tankDriveVolts(0, 0);
                     targetLocked = true;
                 } else { // still not in tolerance, need to rotate
-                    Robot.drivetrain.tankDriveVolts(pivVolts, -pivVolts);
+                    Robot.drivetrain.tankDriveVolts(Robot.limelight.getXOffset() * 0.01 * Constants.Shooter.MAX_PIVOT_VOLTS, -Robot.limelight.getXOffset() * 0.01 * Constants.Shooter.MAX_PIVOT_VOLTS);
                 }
             }
         }
@@ -147,10 +147,10 @@ public class LimelightAutoAim extends CommandBase {
         Robot.indexer.setBeltMotorSpeedPercent(0);
         Robot.indexer.setMidMotorSpeed(0);
 
-        Drivetrain.leftTalon1.enableCurrentLimit(true);
-        Drivetrain.leftTalon2.enableCurrentLimit(true);
-        Drivetrain.rightTalon1.enableCurrentLimit(true);
-        Drivetrain.rightTalon2.enableCurrentLimit(true);
+//        Drivetrain.leftTalon1.enableCurrentLimit(true);
+//        Drivetrain.leftTalon2.enableCurrentLimit(true);
+//        Drivetrain.rightTalon1.enableCurrentLimit(true);
+//        Drivetrain.rightTalon2.enableCurrentLimit(true);
 
         System.out.println("Auto Aim Finished");
         Robot.limelight.setLEDMode(3); // leave LED on after autoaim so we can still use during manual
--- src/main/java/team/gif/robot/subsystems/Drivetrain.java
+++ src/main/java/team/gif/robot/subsystems/Drivetrain.java
@@ -1,7 +1,10 @@
 package team.gif.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -18,18 +21,24 @@ import team.gif.robot.subsystems.drivers.Pigeon;
 
 public class Drivetrain extends SubsystemBase {
 
-    public static WPI_TalonSRX leftTalon1;
-    public static WPI_TalonSRX leftTalon2;
-    public static WPI_TalonSRX rightTalon1;
-    public static WPI_TalonSRX rightTalon2;
+    public static WPI_TalonSRX old_leftTalon1;
+    public static WPI_TalonSRX old_leftTalon2;
+    public static WPI_TalonSRX old_rightTalon1;
+    public static WPI_TalonSRX old_rightTalon2;
+    public static WPI_TalonFX leftTalon1;
+    public static WPI_TalonFX leftTalon2;
+    public static WPI_TalonFX rightTalon1;
+    public static WPI_TalonFX rightTalon2;
 
     public static MotorControllerGroup leftMotors;
     public static MotorControllerGroup rightMotors;
     public static DifferentialDrive drive;
 
     // ------------ Variables for Trajectory ---------------
-    public static WPI_TalonSRX leftEncoderTalon;
-    public static WPI_TalonSRX rightEncoderTalon;
+//    public static WPI_TalonSRX leftEncoderTalon;
+//    public static WPI_TalonSRX rightEncoderTalon;
+    public static WPI_TalonFX leftEncoderTalon;
+    public static WPI_TalonFX rightEncoderTalon;
     public static DifferentialDriveOdometry odometry;
     private static Pigeon pigeon;
     private static int pigeonErrorCount;
@@ -47,10 +56,14 @@ public class Drivetrain extends SubsystemBase {
 
     public Drivetrain() {
         super();
-        leftTalon1 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_LEFT_ONE);
-        leftTalon2 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_LEFT_TWO);
-        rightTalon1 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_RIGHT_ONE);
-        rightTalon2 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_RIGHT_TWO);
+        old_leftTalon1 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_LEFT_ONE);
+        old_leftTalon2 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_LEFT_TWO);
+        old_rightTalon1 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_RIGHT_ONE);
+        old_rightTalon2 = new WPI_TalonSRX(RobotMap.MOTOR_DRIVE_RIGHT_TWO);
+        leftTalon1 = new WPI_TalonFX(16);
+        leftTalon2 = new WPI_TalonFX(17);
+        rightTalon1 = new WPI_TalonFX(18);
+        rightTalon2 = new WPI_TalonFX(19);
 
         leftTalon1.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 20);
         rightTalon1.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 20);
@@ -99,7 +112,7 @@ public class Drivetrain extends SubsystemBase {
         rightTalon1.setInverted(false);
         rightTalon2.setInverted(false);
 
-        pigeon = Robot.isCompBot ? new Pigeon(leftTalon1) : new Pigeon(rightTalon2);
+        pigeon = Robot.isCompBot ? new Pigeon(old_leftTalon1) : new Pigeon(old_rightTalon2);
 
         pigeon.resetPigeonPosition(); // set initial heading of pigeon to zero degrees
 
@@ -109,24 +122,32 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public void currentLimitingSetup(){
-
-        leftTalon1.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
-        leftTalon2.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
-        rightTalon1.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
-        rightTalon2.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
-
-        leftTalon1.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
-        leftTalon2.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
-        rightTalon1.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
-        rightTalon2.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
-
+//        leftTalon1.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
+//        leftTalon2.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
+//        rightTalon1.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
+//        rightTalon2.configContinuousCurrentLimit(MAX_CONTINUOUS_CURRENT_AMPS);
+//
+//        leftTalon1.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
+//        leftTalon2.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
+//        rightTalon1.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
+//        rightTalon2.configPeakCurrentLimit(MAX_PEAK_CURRENT_AMPS);
     }
 
     public void currentLimitingEnable(boolean enableLimit){
-        leftTalon1.enableCurrentLimit(enableLimit);
-        leftTalon2.enableCurrentLimit(enableLimit);
-        rightTalon1.enableCurrentLimit(enableLimit);
-        rightTalon2.enableCurrentLimit(enableLimit);
+//        leftTalon1.enableCurrentLimit(enableLimit);
+//        leftTalon2.enableCurrentLimit(enableLimit);
+//        rightTalon1.enableCurrentLimit(enableLimit);
+//        rightTalon2.enableCurrentLimit(enableLimit);
+
+        leftTalon1.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(enableLimit, 25, 25, 0));
+        leftTalon2.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(enableLimit, 25, 25, 0));
+        rightTalon1.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(enableLimit, 25, 25, 0));
+        rightTalon2.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(enableLimit, 25, 25, 0));
+
+        leftTalon1.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(enableLimit, 80, 80, 0));
+        leftTalon2.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(enableLimit, 80, 80, 0));
+        rightTalon1.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(enableLimit, 80, 80, 0));
+        rightTalon2.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(enableLimit, 80, 80, 0));
     }
 
     // -------------- Teleop Driving -----------------------
@@ -152,8 +173,8 @@ public class Drivetrain extends SubsystemBase {
 
         if (pigeon.isActive()) {
             odometry.update( Rotation2d.fromDegrees(pigeon.get180Heading()),
-                getLeftEncoderPos_Meters(),
-                getRightEncoderPos_Meters());
+                    getLeftEncoderPos_Meters(),
+                    getRightEncoderPos_Meters());
         } else {
             if(++pigeonErrorCount >= 100) { // only print every 2 seconds
                 System.out.println("***   WARNING      \n***\n*** Cannot set robot odometry. Pigeon is not in ready state.\n***\n***");
@@ -178,7 +199,7 @@ public class Drivetrain extends SubsystemBase {
      */
     public DifferentialDriveWheelSpeeds getWheelSpeeds() {
         return new DifferentialDriveWheelSpeeds(leftEncoderTalon.getSelectedSensorVelocity() * (10.0/4096) * Constants.Drivetrain.WHEEL_CIRCUMFERENCE,
-                                                rightEncoderTalon.getSelectedSensorVelocity()* (10.0/4096) * Constants.Drivetrain.WHEEL_CIRCUMFERENCE);
+                rightEncoderTalon.getSelectedSensorVelocity()* (10.0/4096) * Constants.Drivetrain.WHEEL_CIRCUMFERENCE);
     }
 
     /**
@@ -217,11 +238,11 @@ public class Drivetrain extends SubsystemBase {
     public double getRightEncoderPos_Ticks() {
         return rightEncoderTalon.getSelectedSensorPosition();
     }
-    
+
     public double getLeftEncoderVelocity_Ticks() {
         return leftEncoderTalon.getSelectedSensorVelocity();
     }
-    
+
     public double getRightEncoderVelocity_Ticks() {
         return rightEncoderTalon.getSelectedSensorVelocity();
     }
@@ -234,83 +255,83 @@ public class Drivetrain extends SubsystemBase {
     public double getRightEncoderPos_Meters() {
         return rightEncoderTalon.getSelectedSensorPosition() / Constants.Drivetrain.TICKS_TO_METERS_RIGHT;
     }
-    
+
     public double getInputVoltageL1() {
         return leftTalon1.getBusVoltage();
     }
-    
+
     public double getInputVoltageL2() {
         return leftTalon2.getBusVoltage();
     }
-    
+
     public double getInputVoltageR1() {
         return rightTalon1.getBusVoltage();
     }
-    
+
     public double getInputVoltageR2() {
         return rightTalon2.getBusVoltage();
     }
-    
+
     public double getOutputVoltageL1() {
         return leftTalon1.getMotorOutputVoltage();
     }
-    
+
     public double getOutputVoltageL2() {
         return leftTalon2.getMotorOutputVoltage();
     }
-    
+
     public double getOutputVoltageR1() {
         return rightTalon1.getMotorOutputVoltage();
     }
-    
+
     public double getOutputVoltageR2() {
         return rightTalon2.getMotorOutputVoltage();
     }
-    
+
     public double getOutputPercentL1() {
         return leftTalon1.getMotorOutputPercent();
     }
-    
+
     public double getOutputPercentL2() {
         return leftTalon2.getMotorOutputPercent();
     }
-    
+
     public double getOutputPercentR1() {
         return rightTalon1.getMotorOutputPercent();
     }
-    
+
     public double getOutputPercentR2() {
         return rightTalon2.getMotorOutputPercent();
     }
-    
+
     public double getInputCurrentL1() {
         return leftTalon1.getSupplyCurrent();
     }
-    
+
     public double getInputCurrentL2() {
         return leftTalon2.getSupplyCurrent();
     }
-    
+
     public double getInputCurrentR1() {
         return rightTalon1.getSupplyCurrent();
     }
-    
+
     public double getInputCurrentR2() {
         return rightTalon2.getSupplyCurrent();
     }
-    
+
     public double getOutputCurrentL1() {
         return leftTalon1.getStatorCurrent();
     }
-    
+
     public double getOutputCurrentL2() {
         return leftTalon2.getStatorCurrent();
     }
-    
+
     public double getOutputCurrentR1() {
         return rightTalon1.getStatorCurrent();
     }
-    
+
     public double getOutputCurrentR2() {
         return rightTalon2.getStatorCurrent();
     }
